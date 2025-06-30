8430100b8b3ea04e51e353841fe40ee723b6f2f4
Kavinda Jayaweera <kavindajayaweera115@gmail.com>
2025-07-01 01:49:07 +0530

    Feat1 : Core Class Designed

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -11,27 +11,27 @@ public class LoginSteps {
 	public void base_URL() {
 	   LoginPage.optionbrowser();
 	}
-
-	@When("Enter user name {string}")
-	public void enter_user_name(String username) {
-		LoginPage.method_enterUserName(username);
-	}
-
-	@When("Enter password {string}")
-	public void enter_password(String string) {
-		LoginPage.method_enterPassword();
-	}
-
-	@When("Click OK button")
-	public void click_OK_button() {
-		LoginPage.method_ClickOkButton();
-	}
-
-	@Then("Verify the dashboard page title")
-	public void verify_the_dashboard_page_title() {
-		LoginPage.method_VerifyPageTitle();
-	}
-
+//
+//	@When("Enter user name {string}")
+//	public void enter_user_name(String username) {
+//		LoginPage.method_enterUserName(username);
+//	}
+//
+//	@When("Enter password {string}")
+//	public void enter_password(String string) {
+//		LoginPage.method_enterPassword();
+//	}
+//
+//	@When("Click OK button")
+//	public void click_OK_button() {
+//		LoginPage.method_ClickOkButton();
+//	}
+//
+//	@Then("Verify the dashboard page title")
+//	public void verify_the_dashboard_page_title() {
+//		LoginPage.method_VerifyPageTitle();
+//	}
+//
 
 
 }
